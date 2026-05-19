f130fce1ea1e124156f9474755d80c1aad12e499
Your Name <adityapandhare1@gmail.com>
2026-05-20 01:05:30 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,12 @@ const PORT = process.env.PORT || 5000;
 app.use(
     cors({
         origin: process.env.CLIENT_URL || "*",
-        methods:["GET","POST","PUT","DELETE"],
+        methods:["GET","POST","PUT","DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"]
     })
 )
+
+app.option("*", cors())
 
 app.use(express.json());
 app.use("/api/auth",authRouter);
