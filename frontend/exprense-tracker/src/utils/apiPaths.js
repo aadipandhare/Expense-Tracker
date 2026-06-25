70d5c5779c607565a893a63cdbde9bce44c84883
Your Name <adityapandhare1@gmail.com>
2026-06-25 22:53:48 +0530

    g

--- frontend/exprense-tracker/src/utils/apiPaths.js
+++ frontend/exprense-tracker/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://expense-tracker-1jbs.onrender.com"
-export const BASE_URL = "http://localhost:8000"
+export const BASE_URL = "https://expense-tracker-1jbs.onrender.com"
+// export const BASE_URL = "http://localhost:8000"
 
 
 // local backend servers normally run on HTTP, not HTTPS.
